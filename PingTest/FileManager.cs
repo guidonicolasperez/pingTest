87ed84ecf107d41aeb97bf7ab68a61284f2ef160
GuidoNicolasPerez <Guidonicolasperez@gmail.com>
2021-05-12 12:31:08 -0300

    First version.

---
+++ PingTest/FileManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.IO;
+using System.Collections;
+
+
+namespace PingTest
+{
+    public class FileManager
+    {
+
+        public List<string> readFile(string ruta)
+        {
+            // Levanto un List de String para meter todas las lineas y obviamente el fileLine es cada linea.
+            List<string> linesOfFile = new List<string>();
+
+            string fileLine;
+
+            try
+            {
+                // Levanto el archivo
+                StreamReader streamReader = new StreamReader(ruta);
+
+                // Leo la primer linea y si no esta vacia entra en el bucle.
+                fileLine = streamReader.ReadLine();
+
+                // Si entra en el Bucle, me guarda la linea que lei y guarda las siguientes hasta que no haya mas.
+                while (fileLine != null)
+                {
+                    linesOfFile.Add(fileLine);
+
+                    fileLine = streamReader.ReadLine();
+                }
+
+                // Cierro el archivo. (Estoy es mucho muy importante)
+                streamReader.Close();
+
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            return linesOfFile;
+
+        }
+
+        public bool saveFile(List<String> lines, string ruta)
+        {
+            bool ok = false;
+
+            try
+            {
+                StreamWriter streamWriter = new StreamWriter(ruta);
+
+                // escribo las lineas
+                foreach (string line in lines)
+                {
+                    streamWriter.WriteLine(line);
+                }
+
+                streamWriter.Close();
+
+                ok = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                ok = false;
+            }
+
+            return ok;
+        }
+        
+        public bool insertAndSaveFile(string ruta, string line)
+        {
+            bool ok = false;
+
+            List<string> linesOfFile = readFile(ruta);
+
+            linesOfFile.Add(line);
+
+            ok = saveFile(linesOfFile, ruta);
+
+            return ok;
+        }
+
+    }
+}
---
+++ PingTest/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.NetworkInformation;
+
+
+namespace PingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando Ping... ");
+
+            Ping ping = new Ping();
+            PingReply pingReply;
+            string ipAddress = "";
+
+            FileManager FM = new FileManager();
+
+
+
+
+            string pathFileSuccess = Directory.GetCurrentDirectory() + "\\Success\\";
+            string pathFileNotSuccess = Directory.GetCurrentDirectory() + "\\NotSuccess\\";
+
+            string fileName = DateTime.Now.Year + "-"
+                    + DateTime.Now.Month + "-"
+                    + DateTime.Now.Day + "-"
+                    + DateTime.Now.Hour + ".txt";
+
+            bool salida = true;
+
+            List<string> lines = FM.readFile(Directory.GetCurrentDirectory() + "\\ip.txt");
+
+
+            if (lines.Count > 0)
+            {
+                ipAddress = lines[0];
+
+                while (salida)
+                {
+                    Console.WriteLine("Haciendo ping a " + ipAddress);
+
+                    fileName = DateTime.Now.Year + "-"
+                     + DateTime.Now.Month + "-"
+                     + DateTime.Now.Day + "-"
+                     + DateTime.Now.Hour + ".txt";
+
+                    pingReply = ping.Send(ipAddress);
+
+                    if (pingReply.Status.ToString().Equals("Success"))
+                    {
+                        Console.WriteLine(pingReply.Status.ToString() + " " + DateTime.Now);
+                        FM.insertAndSaveFile(pathFileSuccess + "Success in " + fileName, (pingReply.Status.ToString() + " " + DateTime.Now));
+                    }
+                    else
+                    {
+                        Console.WriteLine(pingReply.Status.ToString() + " " + DateTime.Now);
+                        FM.insertAndSaveFile(pathFileNotSuccess + "Not Success in " + fileName, (pingReply.Status.ToString() + " " + DateTime.Now));
+
+                    }
+
+                    System.Threading.Thread.Sleep(1000);
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Ocurrio un error. Revisar Archivo IP.txt");
+            }
+
+        }
+    }
+}
